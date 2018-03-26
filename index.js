95c319d6931852039468a09a6cfbdb1ec2748056
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-26 19:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function takeANumber (katzDeliLine, person) {
   katzDeliLine.push(person)
   return "Welcome, " + person + ". You are number " + katzDeliLine.length + " in line."
 }
+
+function nowServing ( ) {
+  if (katzDeliLine === []) {
+  return 
+  } else {
+    return "the line is empty"
+  }
+}
