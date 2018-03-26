b199a9c96fc8436f311884b70dce77aa3b61093e
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-26 20:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function currentLine (line) {
     var i = line.length
     var myLine = ""
     for (i; i > 0 ; i--) {
+      myLine = (i-(i-1)) + ". " + for ()
     }
     return "The line is currently: " + 
   }
