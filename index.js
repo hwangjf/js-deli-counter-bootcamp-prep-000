30a88c52e48bc6a8aaafb023656aa7a87ea2edea
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-26 19:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeli = [];
 
 function takeANumber (katzDeliLine, person) {
-  katzDeliLine = katzDeli
-  katzDeli.push(person)
  
+  katzDeli.push(person)
+  katzDeliLine = katzDeli
   return "Welcome, " + person + ". You are number " + katzDeli.length + " in line."
 }
