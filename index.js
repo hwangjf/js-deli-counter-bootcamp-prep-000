20fd196c2ccb72a0f814be3d15a44c60621547d8
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-26 20:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function nowServing (katzDeliLine) {
 function currentLine (line) {
   if (line.length === 0) {
     return "The line is currently empty."
+  } else {
+    return "The line is currently : " + line[]
   }
 }
